c54ad540ea2102fb8be33d3d7c605e9b6b779912
evilofskyy <matvey.boroden@gmail.com>
2026-03-20 17:03:45 +0400

    Добавлен класс User и его реализация

---
+++ User.cpp
@@ -0,0 +1,18 @@
+﻿#include "User.h"
+
+User::User(const std::string& _login, const std::string& _password, const std::string& _name) : login(_login), password(_password), name(_name) {}
+
+std::string User::getLogin() const
+{
+	return login;
+}
+
+std::string User::getName() const
+{
+	return name;
+}
+
+bool User::checkPassword(const std::string& _password) const
+{
+	return password == _password;
+}
